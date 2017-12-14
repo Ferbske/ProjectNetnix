1e32a14ddc5a5787d1cf29515f435b9038a260da
Unknown <rick.voesenek@hotmail.com>
2017-12-14 12:23:13 +0100

    Button Homepage nearly done Footer info Colorings

--- src/Netnix/UserInterface.java
+++ src/Netnix/UserInterface.java
@@ -1,9 +1,8 @@
 package Netnix;
 import java.awt.*;
-import java.io.File;
-import javax.imageio.ImageIO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.border.Border;
 
 public class UserInterface implements Runnable {
 
@@ -14,18 +13,19 @@ public class UserInterface implements Runnable {
 
     @Override
     public void run() {
-        mainpage = new JFrame("Netnix Statisticks");
+        mainpage = new JFrame("Netnix Statistix");
         mainpage.setExtendedState(mainpage.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
         mainpage.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        createComponents(mainpage.getContentPane());
+        addDefaultComponents(mainpage.getContentPane());
+        addCenterComponent(mainpage.getContentPane());
 
         mainpage.pack();
         mainpage.setVisible(true);
     }
 
-    private void createComponents(Container container) {
+    public void addDefaultComponents(Container container) {
         container.setLayout(new BorderLayout());
         // NORTH adding
             createNorth(container);
@@ -35,26 +35,45 @@ public class UserInterface implements Runnable {
             createSouth(container);
         // WEST adding
             createWest(container);
+    }
+
+    private void addCenterComponent(Container container){
         // CENTER adding
             createCenter(container);
     }
 
     private void createNorth(Container container){
-        // Image Netnix
-            ImageIcon imageNetnix;
-            JLabel labelNetnix;
             JPanel north = new JPanel();
+        // Button Image
+            ImageIcon imageNetnix;
+            JButton buttonNetnix;
             imageNetnix = new ImageIcon("Images/Netnix.PNG");
-            labelNetnix = new JLabel(imageNetnix);
+            buttonNetnix = new JButton(imageNetnix);
+            buttonNetnix.setContentAreaFilled(false);
+            buttonNetnix.setBorderPainted(false);
+            buttonNetnix.setOpaque(false);
+            buttonNetnix.setFocusPainted(false);
+            buttonNetnix.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    mainpage.setVisible(true);
+                }
+            });
         // Temp testing area's
             north.setBackground(Color.darkGray);
         // Adding all components
-            north.add(labelNetnix);
+            north.add(buttonNetnix);
             container.add(north, BorderLayout.NORTH);
     }
 
     private void createEast(Container container){
         JPanel east = new JPanel();
+        GridLayout layoutEast = new GridLayout(5, 1);
+        east.setLayout(layoutEast);
+        east.add(new JLabel("Grid 1"));
+        east.add(new JLabel("Grid 2"));
+        east.add(new JLabel("Grid 3"));
+        east.add(new JLabel("Grid 4"));
+        east.add(new JLabel("Grid 5"));
         // Temp testing area's
             east.setBackground(Color.green);
         // ADding all components
@@ -63,24 +82,47 @@ public class UserInterface implements Runnable {
 
     private void createSouth(Container container){
         JPanel south = new JPanel();
+        GridLayout layoutSouth = new GridLayout(1, 5);
+        south.setLayout(layoutSouth);
+        south.setPreferredSize(new Dimension(1,20));
+        JLabel blank = new JLabel("");
+        JLabel s1 = new JLabel("Netnix Statistix");
+        s1.setForeground(Color.WHITE);
+        JLabel s2 = new JLabel("Informatica | Jaar 2017 | Klas F | Rik Vanhijfte | Chino Zheng | Rick Voesenek", SwingConstants.RIGHT);
+        s2.setForeground(Color.WHITE);
         // Temp testing area's
-            south.setBackground(Color.blue);
-        // ADding all components
+            south.setBackground(Color.DARK_GRAY);
+        // Adding all components
+            south.add(s1);
+            south.add(blank);
+            south.add(blank);
+            south.add(blank);
+            south.add(s2);
             container.add(south, BorderLayout.SOUTH);
     }
 
     private void createWest(Container container){
         JPanel west = new JPanel();
+        GridLayout layoutWest = new GridLayout(50, 3);
+        west.setLayout(layoutWest);
+        west.setPreferredSize(new Dimension(275,50));
+        for (int i = 0; i < 150; i++){
+            west.add(new JLabel("Grid " + i));
+        }
         // Temp testing area's
-            west.setBackground(Color.red);
+            west.setBackground(Color.gray);
         // ADding all components
             container.add(west, BorderLayout.WEST);
     }
 
     private void createCenter(Container container){
         JPanel center = new JPanel();
+        GridLayout layoutCenter = new GridLayout(50,5);
+        center.setLayout(layoutCenter);
+        for (int i = 0; i < 250; i++)
+            center.add(new JLabel("Grid " + i));
         // Temp testing area's
-            center.setBackground(Color.gray);
+            center.setBackground(Color.lightGray);
         // ADding all components
             container.add(center, BorderLayout.CENTER);
     }
@@ -88,4 +130,6 @@ public class UserInterface implements Runnable {
     public JFrame getFrame() {
         return mainpage;
     }
+
+
 }
